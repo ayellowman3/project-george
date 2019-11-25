2dd217c93a7460f8ab2b8b2d9483d9ed1eb4e3f0
ayellowman3 <ayellowman3@gmail.com>
2019-11-24 22:11:45 -0500

    tried js to maintain external html for header and style

---
+++ main.js
@@ -0,0 +1,8 @@
+document.body.innerHTML = "<meta charset=\"utf-8\">";
+document.body.innerHTML = "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
+document.body.innerHTML = "<link rel=\"stylesheet\" href=\"homestyle.css\">";
+document.body.innerHTML = "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">";
+document.body.innerHTML = "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js\"></script>";
+document.body.innerHTML = "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\"></script>";
+document.body.innerHTML = "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\"></script>";
+document.body.innerHTML = "<link rel=\"shortcut icon\" type=\"image/png\" href=\"./images/g-favicon.png\"/>";
