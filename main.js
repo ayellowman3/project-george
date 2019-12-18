9a914d9ed48036d3e405d54f9d99e76ecbb7fba6
ayellowman3 <ayellowman3@gmail.com>
2019-12-17 21:21:34 -0500

    moved bootstrap to head.html file

--- main.js
+++
@@ -1,9 +0,0 @@
-document.head.innerHTML = "<meta charset=\"utf-8\">";
-document.head.innerHTML = "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
-document.head.innerHTML = "<link rel=\"stylesheet\" href=\"homestyle.css\">";
-document.head.innerHTML = "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">";
-document.head.innerHTML = "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js\"></script>";
-document.head.innerHTML = "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\"></script>";
-document.head.innerHTML = "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\"></script>";
-document.head.innerHTML = "<link rel=\"shortcut icon\" type=\"image/png\" href=\"./images/g-favicon.png\"/>";
-document.head.innerHTML = 'document.head.innerHTML = \'<object type="text/html" data="head.html" ></object>\';'
